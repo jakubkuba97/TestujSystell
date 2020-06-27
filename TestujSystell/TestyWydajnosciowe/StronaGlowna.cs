71eea7185753dd49017e88bbc8868efbb94c4fe8
JakeWindows <jakubkuba997@gmail.com>
2020-06-27 10:12:32 +0200

    Initialised and setup cs project

---
+++ TestujSystell/TestyWydajnosciowe/StronaGlowna.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TestujSystell
+{
+    public class PerfoMainSite
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
